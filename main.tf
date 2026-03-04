3a40272668ee438052c0a95587c0b3ebcf438d70
Sivaram-ssr <sanganilakshmisivaram@gmail.com>
2026-03-04 14:27:59 +0530

    working on terraform

--- main.tf
+++ main.tf
@@ -12,12 +12,13 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
-# resource "aws_subnet" "public" {
-#   count = length(var.public_subnet_cidrs)
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = var.public_subnet_cidrs[count.index]
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
 
-#   tags = {
-#     Name = "Main"
-#   }
-# }
+  tags = {
+    Name = "Main"
+  }
+}
